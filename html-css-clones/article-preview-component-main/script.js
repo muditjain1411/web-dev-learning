2a4ba6fc2a3632bf969c58fafc4e4ca17baee68c
Mudit Jain <muditjain.1406@gmail.com>
2025-08-03 23:57:15 +0530

    Added my existing projects

---
+++ html-css-clones/article-preview-component-main/script.js
@@ -0,0 +1,5 @@
+let share = document.querySelector(".share");
+let popup = document.querySelector(".popup");
+share.addEventListener("click", function () {
+    popup.classList.toggle("active");
+});
